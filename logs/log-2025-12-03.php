b2ffa2ef9665c5ae985eb4c203c3a5ccd547e815
Admin <petratech1830@gmail.com>
2025-12-03 16:53:30 +0700

    galilea v.1.0

---
+++ logs/log-2025-12-03.php
@@ -0,0 +1,59 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-12-03 06:42:14 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:14 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:14 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:14 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:14 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:14 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:14 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:14 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:14 --> 404 Page Not Found: Uploads/system
+ERROR - 2025-12-03 06:42:16 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:16 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:16 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:16 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:16 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:16 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:16 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:16 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:17 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:17 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:17 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:17 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:17 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:17 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:17 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:42:17 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:07 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:07 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:07 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:07 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:07 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:07 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:07 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:07 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:14 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:14 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:14 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:14 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:14 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:14 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:14 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:14 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:48 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:48 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:48 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:48 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:48 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:48 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:48 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 06:43:48 --> 404 Page Not Found: Assets/js
+ERROR - 2025-12-03 13:05:22 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\sttgalilea\application\views\backend\superadmin\permission\list.php 21
+ERROR - 2025-12-03 13:05:22 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\sttgalilea\application\views\backend\superadmin\permission\list.php 21
+ERROR - 2025-12-03 13:05:22 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\sttgalilea\application\views\backend\superadmin\permission\list.php 25
+ERROR - 2025-12-03 13:05:22 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\sttgalilea\application\views\backend\superadmin\permission\list.php 25
+ERROR - 2025-12-03 13:05:22 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\sttgalilea\application\views\backend\superadmin\permission\list.php 29
+ERROR - 2025-12-03 13:05:22 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\sttgalilea\application\views\backend\superadmin\permission\list.php 29
+ERROR - 2025-12-03 13:05:22 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\sttgalilea\application\views\backend\superadmin\permission\list.php 33
+ERROR - 2025-12-03 13:05:22 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\sttgalilea\application\views\backend\superadmin\permission\list.php 33
